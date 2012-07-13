639c16dfe91fe1a2f686e0a6bbc464334cdb8200
Alex Butcher <alex@pebblecode.com>
2012-07-13 18:56:54 +0100

    First working nuget package

--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -14,7 +14,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("4.1.0.1")]
-[assembly: AssemblyFileVersion("4.1.0.1")]
+[assembly: AssemblyVersion("4.1.0.2")]
+[assembly: AssemblyFileVersion("4.1.0.2")]
 
 
