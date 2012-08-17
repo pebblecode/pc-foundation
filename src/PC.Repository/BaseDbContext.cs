e00e72e6dc10060af5002e312b949475a8c87097
Alex Butcher <alex@pebblecode.com>
2012-08-17 15:19:36 +0100

    Added in TransactionProvider and BaseDbContext, to facilitate migration away from static EntityRepository.BeginTransaction

---
+++ src/PC.Repository/BaseDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PebbleCode.Framework.IoC;
+
+namespace PebbleCode.Repository
+{
+    public abstract class BaseDbContext
+    {
+        public RepositoryTransaction BeginTransaction()
+        {
+            return Kernel.Get<TransactionProvider>().BeginTransaction();
+        }
+    }
+
+}
---
+++ src/PC.Repository/TransactionProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PebbleCode.Repository
+{
+    public class TransactionProvider
+    {
+        public virtual RepositoryTransaction BeginTransaction()
+        {
+            return EntityRepository.BeginTransaction();
+        }
+    }
+}
