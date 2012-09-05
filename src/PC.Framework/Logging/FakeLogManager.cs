35ac55309f36075a0f5caea0df6bbb6814fcfe7a
josephjeganathan <josephjeganathan@gmail.com>
2012-09-05 10:12:24 +0100

    Moved FakeLogManager to CT.Framework.Logging

---
+++ src/PC.Framework/Logging/FakeLogManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+
+namespace PebbleCode.Framework.Logging
+{
+    public class FakeLogManager : ILogManager
+    {
+        /// <summary>
+        /// Default to true
+        /// </summary>
+        public bool LogError { get; set; }
+
+        /// <summary>
+        /// Default to true
+        /// </summary>
+        public bool LogWarning { get; set; }
+
+        /// <summary>
+        /// Default to true
+        /// </summary>
+        public bool LogInfo { get; set; }
+        
+        /// <summary>
+        /// Default to true
+        /// </summary>
+        public bool LogDebug { get; set; }
+
+        /// <summary>
+        /// Default to true
+        /// </summary>
+        public bool ThrowExceptionOnError { get; set; }
+
+        /// <summary>
+        /// Default to false
+        /// </summary>
+        public bool ThrowExceptionOnWaring { get; set; }
+
+        public FakeLogManager()
+        {
+            LogError = true;
+            LogWarning = true;
+            LogInfo = true;
+            LogDebug = true;
+            ThrowExceptionOnError = true;
+        }
+
+        public bool ShouldLog(string category, TraceEventType level)
+        {
+            switch (level)
+            {
+                case Logger.ERROR:
+                    return LogError;
+                case Logger.WARNING:
+                    return LogWarning;
+                case Logger.INFO:
+                    return LogInfo;
+                case Logger.DEBUG:
+                    return LogDebug;
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Throws exception when ThrowExceptionOnError|ThrowExceptionOnWaring is set
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="category"></param>
+        /// <param name="level"></param>
+        public void WriteLog(string message, string category, TraceEventType level)
+        {
+            if((ThrowExceptionOnError && level == TraceEventType.Error) ||
+                (ThrowExceptionOnWaring && level == TraceEventType.Warning))
+            {
+                throw new FakeLogManagerLogWrittenException(message, category, level);
+            }
+        }
+    }
+}
---
+++ src/PC.Framework/Logging/FakeLogManagerLogWrittenException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace PebbleCode.Framework.Logging
+{
+    public class FakeLogManagerLogWrittenException : Exception
+    {
+        public FakeLogManagerLogWrittenException()
+        {   
+        }
+
+        public FakeLogManagerLogWrittenException(string message) : base(message)
+        {
+        }
+
+        public FakeLogManagerLogWrittenException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public FakeLogManagerLogWrittenException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {   
+        }
+
+        public FakeLogManagerLogWrittenException(string message, string category, TraceEventType level) : 
+            base(FormatMessage(message, category, level))
+        {
+        }
+
+        private static string FormatMessage(string message, string category, TraceEventType level)
+        {
+            return String.Format("Attempt to write log.\n\rMessage: {0}\r\nCategory: {1}\r\nLevel:{2}", message, category, level);
+        }
+    }
+}
